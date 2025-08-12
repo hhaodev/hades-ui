20024fe3ae2f49c5ff97b5892369e98c8fd2f38d
hhaodev <nghuuhao182001@gmail.com>
2025-08-12 18:30:09 +0700

    demo

--- src/Preview/Dropdown/index.jsx
+++ src/Preview/Dropdown/index.jsx
@@ -28,56 +28,67 @@ const DropdownDemo = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <Stack flex gap={8}>
-      <Dropdown placement="top-start" menu={menu}>
-        <Button theme="default">TOP L</Button>
-      </Dropdown>
-      <Dropdown placement="top" menu={menu}>
-        <Button theme="default">TOP CENTER</Button>
-      </Dropdown>
-      <Dropdown placement="top-end" menu={menu}>
-        <Button theme="default">TOP R</Button>
-      </Dropdown>
-      <Dropdown placement="bottom-start" menu={menu}>
-        <Button theme="default">BOT L</Button>
-      </Dropdown>
-      <Dropdown placement="bottom" menu={menu}>
-        <Button theme="default">BOT CENTER</Button>
-      </Dropdown>
-      <Dropdown placement="bottom-end" menu={menu}>
-        <Button theme="default">BOT R</Button>
-      </Dropdown>
-      <Dropdown menu={menu}>
-        <Button theme="default">NO</Button>
-      </Dropdown>
-      <Dropdown menu={menu} placement="right">
-        <Button theme="default">RIGHT</Button>
-      </Dropdown>
-      <Dropdown menu={menu} placement="left">
-        <Button theme="default">LEFT</Button>
-      </Dropdown>
-      <Dropdown menu={<DateRangePicker />}>
-        <Button theme="default">Open Custom Popup</Button>
-      </Dropdown>
-      <Dropdown
-        open={open}
-        onOpenChange={setOpen}
-        menu={
-          <Select
-            hasSearch
-            options={Array.from({ length: 5 }, (_, i) => {
-              const val = `${i + 1}0000000000000`.toString();
-              return { label: val, value: val };
-            })}
-            placeholder="Select..."
-          />
-        }
-      >
-        <Button theme="default">Open Custom Popup</Button>
-      </Dropdown>
-      <Dropdown menu={<>hehehehehehe</>}>
-        <Button theme="default">Open Custom Popup</Button>
-      </Dropdown>
+    <Stack
+      flexCol
+      gap={8}
+      justify="center"
+      align="center"
+      style={{ height: "calc(100vh - 20px)" }}
+    >
+      <Stack flex gap={8}>
+        <Dropdown placement="top-start" menu={menu}>
+          <Button theme="default">TOP L</Button>
+        </Dropdown>
+        <Dropdown placement="top" menu={menu}>
+          <Button theme="default">TOP CENTER</Button>
+        </Dropdown>
+        <Dropdown placement="top-end" menu={menu}>
+          <Button theme="default">TOP R</Button>
+        </Dropdown>
+      </Stack>
+      <Stack flex gap={8}>
+        <Dropdown placement="bottom-start" menu={menu}>
+          <Button theme="default">BOT L</Button>
+        </Dropdown>
+        <Dropdown placement="bottom" menu={menu}>
+          <Button theme="default">BOT CENTER</Button>
+        </Dropdown>
+        <Dropdown placement="bottom-end" menu={menu}>
+          <Button theme="default">BOT R</Button>
+        </Dropdown>
+      </Stack>
+      <Stack flex gap={8}>
+        <Dropdown menu={menu} placement="left">
+          <Button theme="default">LEFT</Button>
+        </Dropdown>
+        <Dropdown menu={menu} placement="right">
+          <Button theme="default">RIGHT</Button>
+        </Dropdown>
+      </Stack>
+      <Stack flex gap={8}>
+        <Dropdown menu={<DateRangePicker />}>
+          <Button theme="default">Open Custom Popup</Button>
+        </Dropdown>
+        <Dropdown
+          open={open}
+          onOpenChange={setOpen}
+          menu={
+            <Select
+              hasSearch
+              options={Array.from({ length: 5 }, (_, i) => {
+                const val = `${i + 1}0000000000000`.toString();
+                return { label: val, value: val };
+              })}
+              placeholder="Select..."
+            />
+          }
+        >
+          <Button theme="default">Open Custom Popup</Button>
+        </Dropdown>
+        <Dropdown menu={<>hehehehehehe</>}>
+          <Button theme="default">Open Custom Popup</Button>
+        </Dropdown>
+      </Stack>
     </Stack>
   );
 };
